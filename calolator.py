4e458e0e354f1aa1a2bf1b00c9ca25cae218a6ea
omar2012 <10557@wiseschools.edu.eg>
2025-01-28 10:57:34 +0200

    Add files via upload

---
+++ calolator.py
@@ -0,0 +1,11 @@
+1=input("enter num")
+op=input("enter op")
+2=input("enter num")
+if op == "+":
+    print(1 + 2)
+if op == "-":
+    print(1 - 2)
+if op == "*":
+    print(1 * 2) 
+if op == "/":
+    print(1 / 2) 
